6e8e15d6dbb579d4130d25d031c637162b603419
quyencao <boy20195@gmail.com>
2019-07-26 11:18:26 +0700

    Sample commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort
 });
-pgClient.on('error', () => console.log('Lost PG connection'));
+pgClient.on('error', () => console.log('Lost PG connection!!!!!'));
 
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
